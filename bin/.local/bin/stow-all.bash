c33bc1297361f532b762c4e9b898d28d8b5f8a9b
christophe <christophe@bcisoft.fr>
2024-10-09 22:11:22 +0200

    convert nvim-switch to bash

---
+++ bin/.local/bin/stow-all.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if ! command -v stow 2>&1 >/dev/null
+then
+    echo "stow could not be found"
+    exit 1
+fi
+
+cd "$(dirname "$0")"
+ls -1 -d */ | xargs -I '{}' bash -c 'echo {} ; stow {}'
+
