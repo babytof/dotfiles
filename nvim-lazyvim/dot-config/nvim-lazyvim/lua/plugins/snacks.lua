a0961a6f3182fb45706acb0da57193c1194f5ad9
christophe <christophe@bcisoft.fr>
2025-09-19 22:32:17 +0200

    upgrade nvim

--- nvim-lazyvim/dot-config/nvim-lazyvim/lua/plugins/snacks.lua
+++ nvim-lazyvim/dot-config/nvim-lazyvim/lua/plugins/snacks.lua
@@ -1,9 +1,20 @@
 return {
     "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    --@type snacks.Config
     opts = {
-        words = {
-            -- disable highligting same words, perhaps need better config
-            enabled = false,
-        },
+        words = { enabled = false },
+        bigfile = { enabled = true },
+        dashboard = { enabled = true },
+        -- explorer = { enabled = true },
+        indent = { enabled = true },
+        input = { enabled = true },
+        picker = { enabled = true },
+        notifier = { enabled = true },
+        quickfile = { enabled = true },
+        scope = { enabled = true },
+        scroll = { enabled = true },
+        statuscolumn = { enabled = true },
     },
 }
